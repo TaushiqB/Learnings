331cda746a47529d359471c15cd7cb7c3385c5ff
TaushiqB <taushiqbalamurugan@gmail.com>
2025-06-27 00:58:59 +0530

    Dynamic Routing Basics

--- Basic-FastAPI-App/main.py
+++ Basic-FastAPI-App/main.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/")     # Decorator to define the route endpoint - instance of FastAPI - Here "/" is the base path
 def index():
     return {"Data": {"Name": "Taushiq"}}
 
-@app.get("/about")
+# Here get is the operation type (GET request) - @app is the path operation decorator
+@app.get("/about") #Here "/about" is the about path
 def about():
     return {"Data": "This is the about page"}
+
+# Blog system
+@app.get("/blog/{id}")   # Dynamic routing with path parameter
+def show(id):            # Send the id as a path parameter
+    return {"data": id}
+
