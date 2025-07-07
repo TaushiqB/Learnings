76f7e0835ee2acfb3e5fc83ca526093231b37fe4
TaushiqB <taushiqbalamurugan@gmail.com>
2025-07-08 00:25:54 +0530

    Learnt model behaviour

--- pydan/03-Model-Behaviour/examples/Validator.py
+++ pydan/03-Model-Behaviour/examples/Validator.py
@@ -4,3 +4,29 @@ from pydantic import BaseModel, field_validator, model_validator, computed_field
 
 class User(BaseModel):
     Username: str
+
+    @field_validator('username') # Field validators run before the pydantic model - Automatically
+    def username_length(cls, v):
+        if len(v) < 4:
+            raise ValueError("Username must be atleast 4 Characters")
+        return v
+    
+class SignupData(BaseModel):
+    password: str
+    confirm_password: str
+
+    @model_validator(mode='after') # Works after all the custom validations
+    def password_match(cls, values):
+        if values.password != values.confirm_password:
+            raise ValueError('Password Do not Match')
+        return values
+    
+class Product(BaseModel):
+    price: float
+    quantity: int
+
+    @computed_field # Creates a new property
+    @property
+    def total_price(self)->float:
+        return self.price * self.quantity
+
