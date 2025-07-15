bd7eeee217a91102de86773967a9d1005b2cb93a
TaushiqB <taushiqbalamurugan@gmail.com>
2025-07-16 00:48:20 +0530

    Learnt Serialization in Pydantic

--- pydan/05_Serielization/serielization.py
+++ pydan/05_Serielization/serielization.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List
 from datetime import datetime
+
+# Dump and Dump Json - Two types - How these models are used with Pydantic
+# 
 
 class Address(BaseModel):
     street: str
@@ -8,9 +11,41 @@ class Address(BaseModel):
     zip_code: str
 
 class User(BaseModel):
+    id: int
     name: str
     email: str
     is_active: bool = True
     created_At: datetime
     address: Address
     tags: List[str] = []
+
+    model_config = ConfigDict(
+        json_encoders={datetime : lambda v: v.strftime
+        ('%d-%m-%Y %H:%M:%S')}   # This is Used to format The Datetime shit  
+    )
+
+# Create an User Instance 
+user = User(
+     id = 1,
+     name = "Taushiq",
+     email = "taushiwhideh",
+     created_At = datetime(2024, 3, 15, 14, 30),
+     address = Address(
+         street = "bkaajd",
+         city = "Delhi",
+         zip_code = "00"
+     ),
+     is_active = False,
+     tags = ["premium", "subscriber"]
+)
+
+# Actual Serialization Code 
+# Using Model_dump() -> Dictionary is the output
+python_dict = user.model_dump()
+print(python_dict)
+
+# Using Model_dump_json() -> JSON Strings
+json_str = user.model_dump_json()
+print(json_str) # in the strong format the datetime is as - 2024-03-15T14:30:00
+# TO change that we need to write customized model config 
+# This will change due to the change json Encoders
