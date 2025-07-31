a8ca911d551d974d6e2130fa51ae186ff435f450
TaushiqB <taushiqbalamurugan@gmail.com>
2025-08-01 00:01:21 +0530

    Learnt Methods

--- oops-in-python/main.py
+++ oops-in-python/main.py
@@ -1,12 +1,20 @@
 # Each variable has been instantiated from a class. Each Datatype is an object
 class Item:
-    pass
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 item1 = Item() # creating instance of the class
 item1.name = "Tau"
 item1.price = 100
 item1.quantity = 5
+print(item1.calculate_total_price()) # calling the method to calculate total price
 
+item2 = Item() # creating another instance of the class
+item2.name = "T-Shirt"
+item2.price = 50
+item2.quantity = 10
+
+# Displaying the type of each variable
 print(type(item1))
 print(type(item1.name))
 print(type(item1.price))
